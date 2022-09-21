c75cc77e467f1425e92441c8a015122c5e72111d
Raphael Karanja <raphael.karanja@sovtech.com>
2022-09-21 11:58:17 +0300

    feat: setup redux store for authenticating a user

---
+++ src/redux/slices/auth.ts
@@ -0,0 +1,58 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AppThunk } from "../store";
+import { v4 as uuidv4 } from 'uuid';
+import { useNavigate } from "react-router-dom";
+
+interface AuthSliceState { 
+    username: string | null;
+    tabID?: string | null;
+}
+
+const initialState: AuthSliceState = {
+    username: null,
+    tabID: null
+  };
+
+  export const authSLice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+      setUsername: (state, action: PayloadAction<string | null>) => {
+        state.username = action.payload;
+      },
+      setTabId: (state, action: PayloadAction<string | null>) => {
+        state.tabID = action.payload;
+      }
+    },
+  });
+
+  export const {
+    setUsername,
+    setTabId
+  } = authSLice.actions;
+
+
+  export const sendUsername = ({ username }: any): AppThunk => async (dispatch) => {
+    try {
+      let navigate = useNavigate();
+      let tabId = sessionStorage.tabID ? sessionStorage.tabID : uuidv4();
+      let rawUsers: string|null = localStorage.getItem('chat-users');
+      let users: any[] = rawUsers ? JSON.parse(rawUsers) : [];
+
+      users.push({
+        username,
+        tabId
+      });
+
+      localStorage.setItem('chat-users', JSON.stringify(users));
+
+      dispatch(setUsername(username));
+      dispatch(setTabId(tabId));
+
+      navigate("/chat");
+
+
+    } catch (e: any) {
+      console.log("An error occurred")
+    }
+  };
