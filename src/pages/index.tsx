4dd46b14603f6a141617f97e3c22553e9263d990
Raphael Karanja <raphael.karanja@sovtech.com>
2022-09-21 09:58:57 +0300

    feat: cleanup and initialize files and layout

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment, useEffect } from "react";
+
+const Pages = () => {
+    useEffect(() => {
+        document.title = 'Welcome | Chat App';
+    });
+    return (
+        <Fragment>
+
+        </Fragment>    
+    );
+}
+
+export default Pages;
